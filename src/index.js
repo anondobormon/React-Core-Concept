46264026ce2b5cebad9b54d6d4d0df67984faee8
anondobormon <anondokpi@gmail.com>
2021-08-10 00:17:44 +0600

    JSX implementation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const element = React.createElement("h1", null, "Hello Bangladesh!");
+
+// JavaScript to JSX
+const element = (
+  <h1 className="header">
+    <span className="text">Hello Bangladesh!</span>
+  </h1>
 );
+console.log(element);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(element, document.getElementById("root"));
+
+/*
+JavaScript Object
+element = {
+  type: h1,
+  props: {
+    className: "header",
+    children: {
+      type: "span",
+      props: {
+        className: "text",
+        children: "Hello Bangladesh!",
+      },
+    },
+  },
+};*/
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
